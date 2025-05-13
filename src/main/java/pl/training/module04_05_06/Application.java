24f4a6e4296acc583dbec3df10a63f47575c023c
Krzysztof.Glinka <krzysztof.glinka@asseco.pl>
2025-05-13 11:35:58 +0200

    enum currency

---
+++ src/main/java/pl/training/module04_05_06/Application.java
@@ -0,0 +1,10 @@
+package pl.training.module04_05_06;
+
+public class Application {
+    public static void main(String[] args) {
+        var eur = Currency.EUR;
+        var usd = Currency.EUR;
+        System.out.printf("Current currency: %s\n", eur);
+        System.out.printf("Current currency: %s\n", usd);
+    }
+}
